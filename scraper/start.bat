94b0f127588468589963c7c63167cdc48a2d4322
Sakib56 <35700378+Sakib56@users.noreply.github.com>
2020-11-12 02:05:59 +0000

    scraper added

---
+++ scraper/start.bat
@@ -0,0 +1,7 @@
+pip install torch==1.7.0+cpu torchvision==0.8.1+cpu torchaudio===0.7.0 -f https://download.pytorch.org/whl/torch_stable.html
+pip install fastai  
+pip install fastcore
+pip install fastbook
+pip install -Uqq fastbook --user
+pip install jupyter notebook
+jupyter notebook
---
+++ scraper/start.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+pip install torch==1.7.0+cpu torchvision==0.8.1+cpu torchaudio===0.7.0 -f https://download.pytorch.org/whl/torch_stable.html
+pip install fastai  
+pip install fastcore
+pip install fastbook
+pip install -Uqq fastbook --user
+pip install jupyter notebook
+
+sudo easy_install pip
+sudo pip install --upgrade pip
+sudo pip install torch==1.7.0+cpu torchvision==0.8.1+cpu torchaudio===0.7.0 -f https://download.pytorch.org/whl/torch_stable.html
+sudo pip install fastai  
+sudo pip install fastcore
+sudo pip install fastbook
+sudo pip install -Uqq fastbook --user
+sudo pip install jupyter notebook
+
+jupyter notebook
